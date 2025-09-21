d7905838a4e04f492f8e3af4e009c2dc77fbda45
Bruno Diedrich <brunodiedrich97@gmail.com>
2025-09-21 10:36:30 -0300

    fix: create product integration test

--- src/usecase/product/create/create.product.integration.spec.ts
+++ src/usecase/product/create/create.product.integration.spec.ts
@@ -1,5 +1,4 @@
 import { Sequelize } from "sequelize-typescript";
-import Product from "../../../domain/product/entity/product";
 import ProductModel from "../../../infrastructure/product/repository/sequelize/product.model";
 import ProductRepository from "../../../infrastructure/product/repository/sequelize/product.repository";
 import CreateProductUseCase from "./create.product.usecase";
